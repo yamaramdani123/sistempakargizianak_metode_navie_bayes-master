d3ef00a7d5cc656d2403cd4f2c08f58a0c37b732
yamaramdani123 <yamaramdani@gmail.com>
2026-06-10 14:12:42 +0700

    Update sistempakargiziburuk.sql

--- sistempakargiziburuk.sql
+++ sistempakargiziburuk.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2026 at 04:28 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Jun 10, 2026 at 09:11 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -58,17 +58,6 @@ CREATE TABLE `gejala` (
 --
 
 INSERT INTO `gejala` (`id_gejala`, `id_admin`, `id_penyakit`, `nama_gejala`) VALUES
-(1, 1, 1, 'bengkak seluruh tubu'),
-(2, 1, 1, 'rambut terlihat tipis'),
-(3, 1, 1, 'wajah membulat dan sembab'),
-(4, 1, 1, 'pembesaran hati'),
-(5, 1, 1, 'susah makan'),
-(6, 1, 1, 'otot mengecil'),
-(7, 1, 1, 'kelainan kulit'),
-(8, 1, 1, 'anemia'),
-(9, 1, 1, 'mata sayu'),
-(10, 1, 1, 'cengeng dan mudah bawel'),
-(11, 1, 2, 'usus tampak membulat'),
 (12, 1, 2, 'sangat kurus dan wajah membulat'),
 (13, 1, 2, 'perut mencekung dan otot mengecil'),
 (14, 1, 2, 'kulit keriput terjadi kelainan kulit'),
@@ -85,7 +74,22 @@ INSERT INTO `gejala` (`id_gejala`, `id_admin`, `id_penyakit`, `nama_gejala`) VAL
 (25, 1, 3, 'bagian daerah pantat tampak seperti memakai celana longgar'),
 (26, 1, 3, 'berat badan hanya sekitar 60% dari seharusnya'),
 (27, 1, 3, 'otot mengecil'),
-(28, 1, 2, 'nnnnnn');
+(28, 1, 2, 'nnnnnn'),
+(30, 1, 5, 'berat badan sulit naik'),
+(31, 1, 5, 'tubuh terlihat lebih kurus'),
+(32, 1, 5, 'mudah lelah'),
+(33, 1, 5, 'sering sakit'),
+(34, 1, 5, 'pertumbuhan melambat'),
+(35, 1, 4, 'badan sangat kurus (wasting)'),
+(36, 1, 4, 'lemah'),
+(37, 1, 4, 'rambut mudah rontok'),
+(38, 1, 4, 'kulit kering'),
+(39, 1, 4, 'perut bisa tampak buncit'),
+(40, 1, 4, 'serta rentan infeksi'),
+(41, 1, 6, 'tinggi badan lebih pendek dibanding anak seusianya'),
+(42, 1, 6, 'pertumbuhan lambat'),
+(43, 1, 6, 'perkembangan otak dan motorik terlambat'),
+(44, 1, 6, 'serta mudah sakit');
 
 -- --------------------------------------------------------
 
@@ -106,8 +110,7 @@ CREATE TABLE `hasil` (
 --
 
 INSERT INTO `hasil` (`id_hasil`, `id_penyakit`, `namapasien`, `jeniskelamin`, `hasildiagnosa`) VALUES
-(20, 1, 'JOY ', 'Laki-Laki', 'Kwarshiorkor'),
-(22, 1, 'JOY', 'Laki-Laki', 'Kwarshiorkor');
+(23, 3, 'YOSEP', 'Laki-Laki', 'Marasmus');
 
 -- --------------------------------------------------------
 
@@ -126,42 +129,18 @@ CREATE TABLE `nilai` (
 --
 
 INSERT INTO `nilai` (`id_nilai`, `id_penyakit`, `nilai`) VALUES
-(3563, 1, 0.36),
-(3564, 1, 0.36),
-(3565, 1, 0.36),
-(3566, 1, 0.36),
-(3567, 1, 0.36),
-(3568, 1, 0.36),
-(3569, 1, 0.32),
-(3570, 1, 0.32),
-(3571, 1, 0.32),
-(3572, 1, 0.32),
-(3573, 1, 0.32),
-(3574, 1, 0.32),
-(3575, 2, 0.32),
-(3576, 2, 0.32),
-(3577, 2, 0.32),
-(3578, 2, 0.32),
-(3579, 2, 0.32),
-(3580, 2, 0.32),
-(3581, 2, 0.36),
-(3582, 2, 0.32),
-(3583, 2, 0.32),
-(3584, 2, 0.32),
-(3585, 2, 0.32),
-(3586, 2, 0.32),
-(3587, 3, 0.32),
-(3588, 3, 0.32),
-(3589, 3, 0.32),
-(3590, 3, 0.32),
-(3591, 3, 0.32),
-(3592, 3, 0.32),
-(3593, 3, 0.32),
-(3594, 3, 0.36),
-(3595, 3, 0.36),
-(3596, 3, 0.36),
-(3597, 3, 0.36),
-(3598, 3, 0.36);
+(3611, 2, 0.52),
+(3612, 2, 0.52),
+(3613, 2, 0.48),
+(3614, 2, 0.48),
+(3615, 2, 0.48),
+(3616, 2, 0.48),
+(3617, 3, 0.48),
+(3618, 3, 0.48),
+(3619, 3, 0.52),
+(3620, 3, 0.52),
+(3621, 3, 0.52),
+(3622, 3, 0.52);
 
 -- --------------------------------------------------------
 
@@ -180,9 +159,8 @@ CREATE TABLE `nilai_akhir` (
 --
 
 INSERT INTO `nilai_akhir` (`id_nilaiakhir`, `id_penyakit`, `nilai_akhir`) VALUES
-(343, 1, 0.0000007791),
-(344, 2, 0.000000432345),
-(345, 3, 0.000000692533);
+(350, 2, 0.00478465),
+(351, 3, 0.00561532);
 
 -- --------------------------------------------------------
 
@@ -205,7 +183,7 @@ CREATE TABLE `pasien` (
 --
 
 INSERT INTO `pasien` (`id_pasien`, `id_admin`, `nama_pasien`, `usernameuser`, `jk`, `usia`, `password_user`) VALUES
-(1, 1, 'budi', 'budi', 'Laki-Laki', '5 Tahun', '123');
+(6, 1, 'YOSEP', 'YOSEP', 'Laki-Laki', '20 tahun', 'YOSEP');
 
 -- --------------------------------------------------------
 
@@ -225,9 +203,11 @@ CREATE TABLE `penyakit` (
 --
 
 INSERT INTO `penyakit` (`id_penyakit`, `id_pasien`, `nama_penyakit`, `solusi`) VALUES
-(1, 1, 'Kwarshiorkor', 'tesing'),
 (2, 1, 'Marasmik-Kwarshiorkor', 'testing2'),
-(3, 1, 'Marasmus', 'Testing 3');
+(3, 1, 'Marasmus', 'Testing 3'),
+(4, 1, 'Gizi Buruk', 'Memberikan asupan makanan tinggi energi dan protein, disertai penanganan medis '),
+(5, 1, 'gizi kurang', 'Meningkatkan konsumsi makanan bergizi seimbang,'),
+(6, 1, 'Stunting', 'mpasi');
 
 --
 -- Indexes for dumped tables
@@ -295,37 +275,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `gejala`
 --
 ALTER TABLE `gejala`
-  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `hasil`
 --
 ALTER TABLE `hasil`
-  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3599;
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3623;
 
 --
 -- AUTO_INCREMENT for table `nilai_akhir`
 --
 ALTER TABLE `nilai_akhir`
-  MODIFY `id_nilaiakhir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=346;
+  MODIFY `id_nilaiakhir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=352;
 
 --
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `penyakit`
 --
 ALTER TABLE `penyakit`
-  MODIFY `id_penyakit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_penyakit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
